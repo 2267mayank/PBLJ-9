9f405648e3900446056a0f5b74d46426df0adf9d
2267mayank <mayankraj100002@gmail.com>
2025-11-06 20:30:59 +0530

    Add HibernateUtil class for session management

---
+++ HibernateUtil.java
@@ -0,0 +1,27 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .addAnnotatedClass(Account.class)
+                .buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
